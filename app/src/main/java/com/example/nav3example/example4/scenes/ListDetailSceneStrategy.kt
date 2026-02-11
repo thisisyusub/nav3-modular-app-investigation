34804fe32e23de595cdb58210fe33cf96c67aeee
Thisisyusub <kanan.yusubov@abb-bank.az>
2026-02-11 16:44:58 +0400

    example 4 provided for scenes

--- app/src/main/java/com/example/nav3example/example4/scenes/ListDetailSceneStrategy.kt
+++ app/src/main/java/com/example/nav3example/example4/scenes/ListDetailSceneStrategy.kt
@@ -6,6 +6,8 @@ import androidx.navigation3.scene.SceneStrategy
 import androidx.navigation3.scene.SceneStrategyScope
 import androidx.window.core.layout.WindowSizeClass
 import androidx.window.core.layout.WindowSizeClass.Companion.WIDTH_DP_MEDIUM_LOWER_BOUND
+import com.example.nav3example.example4.scenes.ListDetailScene.Companion.DETAIL_KEY
+import com.example.nav3example.example4.scenes.ListDetailScene.Companion.LIST_KEY
 
 class ListDetailSceneStrategy<T : Any>(
     val windowSizeClass: WindowSizeClass,
@@ -15,18 +17,17 @@ class ListDetailSceneStrategy<T : Any>(
             return null
         }
 
-        val detailEntry = entries
-            .lastOrNull()
-            ?.takeIf { it.metadata.containsKey(ListDetailScene.DETAIL_KEY) }
+        val detailEntry =
+            entries.lastOrNull()?.takeIf { it.metadata.containsKey(DETAIL_KEY) } ?: return null
 
-        val listEntry = entries
-            .findLast { it.metadata.containsKey(ListDetailScene.LIST_KEY) }
+        val listEntry = entries.findLast { it.metadata.containsKey(LIST_KEY) } ?: return null
 
 
         return ListDetailScene(
             list = listEntry,
             detail = detailEntry,
             key = listEntry.contentKey,
+            previousEntries = entries.dropLast(1),
         )
     }
 }
